c9ee12b4dca241b79f29c0e94663877ad9e7378e
Abdelrhman mikky <73851000+mikky422@users.noreply.github.com>
2022-08-15 21:13:05 +0200

    Add files via upload

---
+++ Training/Tasks.js
@@ -0,0 +1,24 @@
+
+/*-------------First Task-----------------*/
+
+/*let salary = window.prompt("Enter your salary");
+console.log(salary);
+
+const TaxRatio = salary * 0.1;
+let netsalary = salary - TaxRatio;
+console.log(net1salary);*/
+
+/*-------------Second Task-----------------*/
+
+let num1 = parseInt(prompt("enter first number"));
+let num2 = parseInt(prompt("enter Second number"));
+
+let sum = num1 + num2;
+let minus = num1 - num2;
+let multiplie = num1 * num2;
+let divide = num1 / num2;
+
+console.log(sum);
+console.log(minus);
+console.log(multiplie);
+console.log(divide);
